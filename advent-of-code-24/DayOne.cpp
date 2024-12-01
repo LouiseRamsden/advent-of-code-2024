e0a809881ad4c61f1182a8f2e786f4b7e2550f69
NoteBot <louiseramsden2@gmail.com>
2024-12-01 10:20:03 +0000

    Commenting

--- advent-of-code-24/DayOne.cpp
+++ advent-of-code-24/DayOne.cpp
@@ -8,26 +8,24 @@ DayOne::TwinVectors DayOne::splitInput(std::ifstream& in)
 	std::string str;
 	TwinVectors outStruct = {};
 
-
-	//Splits each column into two separate vectors same as first part
+	//Splits each column into two separate vectors in the TwinVector Struct
 	while (std::getline(in, str))
 	{
 		std::stringstream ss(str);
 		std::string temp;
 		std::getline(ss, temp, ' ');
 		outStruct.rowA.push_back(stoi(temp));
-		std::getline(ss, temp, ' ');
-		std::getline(ss, temp, ' ');
-		std::getline(ss, temp, ' ');
+		std::getline(ss, temp, ' ');//Gross
+		std::getline(ss, temp, ' ');//Gross
+		std::getline(ss, temp, ' ');//Gross
 		outStruct.rowB.push_back(stoi(temp));
 	}
-
 	return outStruct;
 }
 
 uint32_t DayOne::p1(std::ifstream& in) 
 {
-	
+	//Make both vectors
 	TwinVectors vecs = splitInput(in);
 	
 	//sorts both vector
@@ -36,6 +34,7 @@ uint32_t DayOne::p1(std::ifstream& in)
 
 	uint32_t result = 0;
 
+	//adds the absolute value of the difference of each cell of two sorted vectors to the result
 	for (int i = 0; i < vecs.rowA.size(); i++) 
 	{
 		result += std::abs(vecs.rowA[i] - vecs.rowB[i]);
@@ -47,9 +46,12 @@ uint32_t DayOne::p1(std::ifstream& in)
 
 uint32_t DayOne::p2(std::ifstream& in) 
 {
+	//Make both vectors
 	TwinVectors vecs = splitInput(in);
 
 	uint32_t result = 0;
+
+	//Multiplies RowA by the amount of times it occurs in row B then adds to result
 	for (int i = 0; i < vecs.rowA.size(); i++)
 	{
 		result += vecs.rowA[i] * std::count(vecs.rowB.begin(), vecs.rowB.end(), vecs.rowA[i]);
